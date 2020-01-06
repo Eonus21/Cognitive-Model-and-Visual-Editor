4d56c0b3072b630a0384de410ee6420ba29a4f49
Alexey <49307215+IIE4EHbKAKOKC@users.noreply.github.com>
2020-01-06 10:07:34 +0200

    update

--- visual_editor/MainPanelCreator.java
+++ visual_editor/MainPanelCreator.java
@@ -2,11 +2,15 @@ package visual_editor;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import java.awt.Canvas;
 
 public class MainPanelCreator {
     private JPanel panel;
     public MainPanelCreator() {
         this.panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        Canvas canvas = new Canvas ();
+        canvas.setSize(700,300);
+        panel.add(canvas);
     }
 }
